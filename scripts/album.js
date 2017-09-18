1f34e56f9c77b2dbec6e27dfe36184720012eb18
nickantes <nick.c.antes@gmail.com>
2017-09-17 20:12:30 -0700

    testing bugs

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,13 @@
 var setSong = function(songNumber) {
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
-};
+
+
+currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+  formats: [ 'mp3' ],
+        preload: true
+    });
+    };
 
 var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
